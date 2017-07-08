26e33b81d75605125c6b345deee3708068c742b5
Oliver-Sutton <osutton2010@gmail.com>
2017-07-08 20:27:19 +0100

    added get request for summoner information

--- src/com/oliversutton/lolinfo/SearchSummoner.java
+++ src/com/oliversutton/lolinfo/SearchSummoner.java
@@ -2,6 +2,13 @@ package com.oliversutton.lolinfo;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
@@ -13,6 +20,8 @@ public class SearchSummoner implements ActionListener{
 	private JComboBox<?> comboRegion;
 	private JTextField txtSummonerName;
 	private JLabel lblUserFeedback;
+	private int summonerId;
+	private String apiKey = getApiKey();
 	
 	// Gets the 3 elements that will be needed to search summoner id and also allow for user feedback.
 	public SearchSummoner(JComboBox<?> comboRegion, JTextField txtSummonerName, JLabel lblUserFeedback) {
@@ -29,7 +38,55 @@ public class SearchSummoner implements ActionListener{
 			this.lblUserFeedback.setText("Please select a region."); // Gives the user feedback, telling them they need to select a region.
 			return;
 		}
-		this.lblUserFeedback.setText(""); // resets text to blank if conditions are met and it had alreayd been triggered.
+		this.lblUserFeedback.setText(""); // resets text to blank if conditions are met and it had already been triggered.
+		try {
+			getSummonerId(this.txtSummonerName.getText());
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
 	}
+	
+	private String getApiKey(){
+		String filePath = "D:\\Java\\LoLInfo\\apiKey.txt";
+		String apiKey = "";
+		BufferedReader readerBuffer = null;
+		try { readerBuffer = new BufferedReader(new FileReader(filePath)); } catch (FileNotFoundException e) { e.printStackTrace(); }
+		try { apiKey = readerBuffer.readLine(); } catch (IOException e) { e.printStackTrace(); }
+		try { readerBuffer.close(); } catch (IOException e) { e.printStackTrace(); }
+		return apiKey;
+	}
+	
+	private void getSummonerId(String summonerName) throws IOException{
+		
+		String server;
+		
+		switch ((String)this.comboRegion.getSelectedItem()) {
+		case "EUW":
+			server = "euw1";
+			break;
+		case "NA":
+			server = "na";
+			break;
+		default:
+			server = "euw1";
+			break;
+		}
+		
+		String requestString = "https://" + server + ".api.riotgames.com/lol/summoner/v3/summoners/by-name/" 
+								+ this.txtSummonerName.getText() + "?api_key=" + this.apiKey;
+		
+		URL url = new URL(requestString);
+		URLConnection connection = url.openConnection();
+		connection.connect();
+		
+		BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+		String returnedInfo;
+		
+		while((returnedInfo = in.readLine()) != null) {
+			System.out.println(returnedInfo);
+		}
+		
+		in.close();
+	} 
 	
 }
