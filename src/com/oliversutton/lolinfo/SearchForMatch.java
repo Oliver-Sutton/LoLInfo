a8df33eb4793029e735d175647e30fcb3a6595e0
Oliver-Sutton <osutton2010@gmail.com>
2017-07-15 15:33:33 +0100

    began implementation of searchforgame

--- src/com/oliversutton/lolinfo/SearchForMatch.java
+++ src/com/oliversutton/lolinfo/SearchForMatch.java
@@ -2,9 +2,15 @@ package com.oliversutton.lolinfo;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 public class SearchForMatch implements ActionListener{
 	
@@ -26,6 +32,30 @@ public class SearchForMatch implements ActionListener{
 		URL url = new URL(requestString);
 		URLConnection connection = url.openConnection();
 		connection.connect();
+		
+		BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+		String returnedInfo;
+		
+		while((returnedInfo = in.readLine()) != null) {
+			JSONParser parser = new JSONParser();
+			try {
+				Object object = parser.parse(returnedInfo);
+				
+				JSONObject jsonObject = (JSONObject)object;
+				
+				JSONArray playerArray = (JSONArray)jsonObject.get("participants");
+				
+				for(int i = 0; i != playerArray.toArray().length; i++) {
+					JSONObject playerInfo = (JSONObject)playerArray.get(i);
+					String summonerName = (String) playerInfo.get("summonerName");
+					System.out.println(summonerName);
+				}
+				
+				
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
 		
 	}
 	
